2ddc8e18bff549b17bd07ec6300a1eded87f6ac7
k6pede <runa720.bump@icloud.com>
2023-09-06 21:21:58 +0900

    Initialize basic task scheduler setup

---
+++ app/Models/EmailSubscriber.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmailSubscriber extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'user_id';
+
+    protected $keyType = 'unsignedBigInteger';
+
+    public $incrementing = false;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+}
